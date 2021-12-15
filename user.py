1ad0543c2abfaf20d5a673e6204e107c0a746c8e
raushanraja <vraushanraja@gmail.com>
2021-12-15 11:06:26 +0530

    use wrapper

--- user.py
+++ user.py
@@ -2,6 +2,20 @@ from db.core.session import SessionLocal
 from db.crud import crud_user
 from db.model.user import DB
 
-with SessionLocal() as db:
+
+def session_wrapper(func):
+    def wrapper(*args, **kwargs):
+        with SessionLocal() as db:
+            func(db=db)
+
+    return wrapper
+
+
+@session_wrapper
+def get_user(*args, **kwargs):
+    db = kwargs['db']
     res = crud_user.get_by_email(db=db, email="admin@admin.com")
-    print(res.asjson(DB,ignore_fields=['id']))
+    if res is not None:
+        print(res.asjson(DB, ignore_fields=['id']))
+
+get_user()
